11c553c2ff9431b94afdb9f5ebf7514d50d12aba
Tanvir-yzu <2020tanvir1971@gmail.com>
2026-07-27 09:45:16 +0800

    chore: add initial hello world go application
    create go module config and main.go with basic variable printing example

---
+++ hello_word/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string = "TANVIR"
+	var age int = 25
+	country := "Bangladesh"
+	isStudent := true
+
+	fmt.Println(name, age, country, isStudent)
+
+
+
+}
